78232f5b97243233c637122382d6725694c2214f
can-ek <can-ek@github.com>
2025-12-14 15:07:00 -0800

    Adding code to aggregate RSS feeds

--- cmdAgg.go
+++ cmdAgg.go
@@ -1,17 +1,62 @@
 package main
 
 import (
-	rss "github.com/can-ek/gator/rss"
-	"fmt"
 	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	db "github.com/can-ek/gator/internal/database"
+	rss "github.com/can-ek/gator/rss"
 )
 
 func handleAgg(s *state, cmd command) error {
-	feed, err :=  rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("agg expects time_between_reqs argument")
+	}
+
+	duration, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(feed)
+	fmt.Println("Collecting feeds every", duration)
+
+	ticker := time.NewTicker(duration)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	ctx := context.Background()
+	feed, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return err
+	}
+
+	currTime := time.Now()
+	params := db.MarkFeedFetchedParams{
+		LastFetchedAt: sql.NullTime{
+			Time:  currTime,
+			Valid: true,
+		},
+		ID: feed.ID,
+	}
+
+	feed, err = s.db.MarkFeedFetched(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	rssFeed, err := rss.FetchFeed(ctx, feed.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Println(item.Title)
+	}
+
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -15,3 +15,14 @@ SELECT * FROM feeds;
 
 -- name: GetFeedsByUrl :one
 SELECT * FROM feeds WHERE url = $1;
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = $1, updated_at = $1
+WHERE id = $2
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
